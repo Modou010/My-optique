692dbb28d95088a0686c55a2b22aa51dc1c77c2d
Modou010 <127099056+Modou010@users.noreply.github.com>
2024-04-16 18:21:23 +0200

    modification du fichier model.php

--- projet php/Model/identifiant_db.php
+++ projet php/Model/identifiant_db.php
@@ -1,12 +1,8 @@
 <?php
-// $host = "localhost";
-// $login = "root";
-// $password = "";
-// $dbname = "projet_php";
+$host = "localhost";
+$login = "root";
+$password = "";
+$dbname = "projet_php";
 
-$host = "mysql02.univ-lyon2.fr";
-$login = "php_mmboup";
-$password = "PG5OFFoc8LQJg6jlkUO5lpVGS";
-$dbname = "php_mmboup";
 
 ?>
