32e9c099936150df898c3424e811df2755864dc6
MBOUP Modou <mboupmodou05@gmail.com>
2024-12-09 10:46:06 +0100

    ok

--- projet php/Model/identifiant_db.php
+++ projet php/Model/identifiant_db.php
@@ -1,8 +1,11 @@
 <?php
-$host = "localhost";
-$login = "root";
-$password = "";
-$dbname = "projet_php";
+// $host = "localhost";
+// $login = "root";
+// $password = "";
+// $dbname = "projet_php";
 
-
+$host = "monserveur001.mysql.database.azure.com"; // Remplace par le nom de ton serveur Azure
+$dbname = "bdd_site";               // Nom de ta base de données
+$login = "mboup_modou";                    // Nom d'utilisateur SQL
+$password = "Nopolicepl1@(o-o)";           // Mot de passe SQL
 ?>
